07b7797c5c9c13511cb47b1b6185a68fe672e162
nilsbjorndahl02 <54797888+nilsbjorndahl02@users.noreply.github.com>
2019-09-22 21:04:32 +0200

    del 1

---
+++ Nils Eclipse/programmering/hemläxa/vecka38.java
@@ -0,0 +1,21 @@
+package hemläxa;
+
+import java.util.Scanner;
+
+public class vecka38 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("skriv in ett heltal?");
+		int a = input.nextInt();
+		int max = a;
+		int min = a;
+		System.out.println("skriv in ett till heltal");
+		int b =input.nextInt();
+		if(b>max) {
+			
+		}
+	}
+
+}
